1c00d8c2b6fd92770f49c712b6577b380285f672
Ellie <kloberdanz.eliska@gmail.com>
2021-03-18 14:24:16 -0500

    implemented loss

--- NeuralNet.hpp
+++ NeuralNet.hpp
@@ -184,4 +184,72 @@ void ActivationSoftmax::backward(Eigen::MatrixXd dvalues) {
 //         return data_loss
 //         # Cross-entropy loss
 
+    // # Forward pass
+    // def forward(self, y_pred, y_true):
+    //     # Number of samples in a batch
+    //     samples = len(y_pred)
+    //     # Clip data to prevent division by 0
+    //     # Clip both sides to not drag mean towards any value
+    //     y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+    //     # Probabilities for target values - only if categorical labels
+    //     if len(y_true.shape) == 1:
+    //       correct_confidences = y_pred_clipped[range(samples),y_true]
+    //     # Mask values - only for one-hot encoded labels
+    //     elif len(y_true.shape) == 2: 
+    //       correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+    //     # Losses
+    //     negative_log_likelihoods = -np.log(correct_confidences)
+    //     return negative_log_likelihoods
+
+    //     # Backward pass
+    // def backward(self, dvalues, y_true):
+    //     # Number of samples
+    //     samples = len(dvalues)
+    //     # Number of labels in every sample We’ll use the first sample to count them
+    //     labels = len(dvalues[0])
+    //     # If labels are sparse, turn them into one-hot vector
+    //     if len(y_true.shape) == 1:
+    //       y_true = np.eye(labels)[y_true]
+    //     # Calculate gradient
+    //     self.dinputs = -y_true / dvalues
+    //     # Normalize gradient
+    //     self.dinputs = self.dinputs / samples
+
+
+class Loss {
+    public:
+        Eigen::MatrixXd dinputs;
+
+        // Member functions declaration
+        Eigen::VectorXd calculate(Eigen::VectorXd output, Eigen::VectorXd y);
+        Eigen::VectorXd forward(Eigen::VectorXd y_pred, Eigen::VectorXd y_true);
+        void backward(Eigen::VectorXd dvalues, Eigen::VectorXd y_true);
+};
+
+// Loss& loss();
+
+// Member functions definitions
+Eigen::VectorXd Loss::forward(Eigen::VectorXd y_pred, Eigen::MatrixXd y_true) {
+    int samples = y_pred.size();
+    //if y_true.rows() 
+    Eigen::MatrixXd correct_confidences = (y_pred, y_true).colwise().sum();
+    Eigen::MatrixXd negative_log_likelihoods = correct_confidences.array().log();
+    return negative_log_likelihoods;
+}
+
+void Loss::backward(Eigen::VectorXd dvalues, Eigen::VectorXd y_true) {
+    int samples = dvalues.size();
+    dinputs = - (y_true.array().rowwise()/dvalues);
+    dinputs = dinputs/samples;
+
+}
+
+Eigen::VectorXd Loss::calculate(Eigen::VectorXd output, Eigen::VectorXd y) {
+    Eigen::VectorXd sample_losses = Loss::forward(output, y);
+    Eigen::VectorXd data_loss = sample_losses.rowwise().mean();
+    return sample_losses;
+}
+
+
+
 #endif // NEURALNET_HPP
